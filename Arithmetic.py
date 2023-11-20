a8825f85f20016b3151ccb83539baaa69af2c828
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-20 14:40:34 +0800

    Create Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,20 @@
+num1 = 75
+num2 = 50
+
+#addition
+sum = num1 + num2
+
+#subtraction
+diff = num1 - num2
+
+#multiplication
+product = num1 * num2
+
+#division
+quotient = num1 / num2
+
+#results
+print("The sum of", num1, "and", num2, "is", sum)
+print("The difference of", num1, "and", num2, "is", diff)
+print("The product of", num1, "and", num2, "is", product)
+print("The quotient of", num1, "and", num2, "is", quotient)
