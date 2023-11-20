66012128fd2b0e3bb7ba4becd83573d559086e87
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-20 15:18:42 +0800

    Create for-loop.py

---
+++ for-loop.py
@@ -0,0 +1,9 @@
+#Exercise 4 for loops
+#Create a python program that displays all even number from 1 to 100 using for loops
+
+for number in range(1,100):
+
+  if number % 2 == 0:
+    print(number)
+
+#To show even number change line 6 to "if number % 2 != 0:"
