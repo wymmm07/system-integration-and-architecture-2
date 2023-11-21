7749c1d26478818157821f0c9751401509f36241
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-21 15:10:44 +0800

    Update odd-or-even-fucntion.py

--- odd-or-even-fucntion.py
+++ odd-or-even-fucntion.py
@@ -1,16 +1,9 @@
-#Exercise 8: odd or even functions.py
+#Exercise 7: Odd_or_even_functions.py
 
-def print_even_numbers():
-  for i in range(1, 100):
-      if i % 2 == 0:
-          print(i)
-def print_odd_numbers():
-  for i in range(1, 100):
-      if i % 2 != 0:
-          print(i)
-
-value = input("odd or even? ")
-if value == "odd":
-  print_odd_numbers()
-elif value == "even":
-  print_even_numbers()
+def even_or_odd():
+    value = input("What Number? ")
+    if int(value) % 2 == 0:
+        print(f"{value} is even")
+    else:
+        print(f"{value} is odd")
+even_or_odd()
