cfc2c02f9ebcc2d3cd3b3840b28773938486a947
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-21 15:02:09 +0800

    Create for-loops-function.py

---
+++ for-loops-function.py
@@ -0,0 +1,12 @@
+#Exercise 8: For_loops_functions.py
+
+def print_even_numbers():
+  for i in range(1, 100):
+      if i % 2 == 0:
+          print(i)
+def print_odd_numbers():
+  for i in range(1, 100):
+      if i % 2 != 0:
+          print(i)
+print_even_numbers()
+print_odd_numbers()
