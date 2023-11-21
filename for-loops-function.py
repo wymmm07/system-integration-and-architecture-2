233b5cb59082c5c6923a084215d2b97fde0c41cc
Wym <145339126+wymmm07@users.noreply.github.com>
2023-11-21 15:24:46 +0800

    Update for-loops-function.py

--- for-loops-function.py
+++ for-loops-function.py
@@ -1,12 +1,16 @@
 #Exercise 8: For_loops_functions.py
 
-def print_even_numbers():
+def even_numbers():
   for i in range(1, 100):
       if i % 2 == 0:
           print(i)
-def print_odd_numbers():
+def odd_numbers():
   for i in range(1, 100):
       if i % 2 != 0:
           print(i)
-print_even_numbers()
-print_odd_numbers()
+
+value = input("Odd or Even? ")
+if value == "odd":
+  print_odd_numbers()
+elif value == "even":
+  print_even_numbers()
